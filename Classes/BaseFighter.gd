cd0f60e8012212282286b48f82b4c120182fd61e
azreigh <noctaliavibing@gmail.com>
2022-09-24 18:32:38 +0100

    Redo everything in 2d and add solid tiles

--- Classes/BaseFighter.gd
+++ Classes/BaseFighter.gd
@@ -1,69 +1,76 @@
-extends CharacterBody3D
-class_name BaseFighter
+extends CharacterBody2D
 
-var player_id = 0
+const SPEED = 150.0
+const RUN_SPEED = 230.0
+const DODGE_SPEED = 300.0
+const JUMP_VELOCITY = -234.0
+const GRAVITY = 950
+const MAX_Y_VELOCITY = 400
+const MAX_JUMPS = 2
+const SECONDARY_JUMP_MOD = 0.9
 
-var _run_speed = 5.0
-var _run_speed_mult = 1.5
-var _jump_velocity = 4.5
+var jumps: int = MAX_JUMPS
+var running: bool = true
+var dodging: bool = true
+var dodge_started_on_ground: bool = false
+var can_dodge: bool = true
+var passthrough_platforms: bool = true
+var old_velocity: Vector2 = Vector2.ZERO
 
-var _max_jumps = 2
-var _current_jumps = 2
-
-const DODGE_SPEED = 15
-
-var _can_dodge = true
-var _running = true
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = 9.8
-
-# Timers
-@onready var _can_move_timer = Timer.new()
+var dodge_timer = Timer.new()
 
 func _ready():
-	add_child(_can_move_timer)
-	_can_move_timer.one_shot = true
+	dodge_timer.one_shot = true
+	add_child(dodge_timer)
 
 func _physics_process(delta):
-	if _can_move_timer.time_left > 0.0:
-		if _can_move_timer.time_left > 0.06 and is_on_floor():
-			_can_move_timer.stop()
-			_can_move_timer.start(0.06)
-		if _can_move_timer.time_left > 0.10:
-			velocity.x *= 0.85
-			velocity.y *= 0.85
-		move_and_slide()
-		return
-	
-	if not is_on_floor():
-		velocity.y -= gravity * delta
-	else:
-		_can_dodge = true
-		_current_jumps = _max_jumps
+	if dodge_timer.time_left == 0.0:
+		if dodging: dodging = false
+		passthrough_platforms = Input.is_action_pressed("ui_down")
+		# Add the gravity.
+		if not is_on_floor():
+			velocity.y = min(velocity.y + (GRAVITY * delta), MAX_Y_VELOCITY * (1.5 if passthrough_platforms else 1.0))
+		elif jumps != MAX_JUMPS:
+			jumps = MAX_JUMPS
+		
+		if is_on_floor():
+			can_dodge = true
+		
+		if is_on_floor() and passthrough_platforms:
+			position.y += 1
+			velocity.y += (GRAVITY * delta)
+			velocity = old_velocity
+		# Handle Jump.
+		if Input.is_action_just_pressed("jump") and jumps > 0:
+			velocity.y = JUMP_VELOCITY * (1.0 if jumps == MAX_JUMPS else SECONDARY_JUMP_MOD)
+			jumps -= 1
 
-	# Handle Jump.
-	if Input.is_action_just_pressed("jump") and _current_jumps > 0:
-		velocity.y = _jump_velocity
-		_current_jumps -= 1
-	
-	if Input.is_action_pressed("dodge") and _can_dodge:
-		var dodge_dir = Input.get_vector("ui_left", "ui_right", "ui_down", "ui_up")
-		if !dodge_dir.is_zero_approx():
-			velocity.x = dodge_dir.x * DODGE_SPEED
-			velocity.y = dodge_dir.y * DODGE_SPEED
-			_can_dodge = false
-			_can_move_timer.start(0.35)
-	else:
-		if !is_on_floor() and Input.is_action_just_released("jump") and velocity.y > 0:
-			velocity.y *= 0.3
 		# Get the input direction and handle the movement/deceleration.
 		# As good practice, you should replace UI actions with custom gameplay actions.
-		var input_dir = Input.get_vector("ui_left", "ui_right", "ui_down", "ui_up")
-		var direction = (transform.basis * Vector3(input_dir.x, 0, -input_dir.y)).normalized()
-		if direction:
-			velocity.x = direction.x * _run_speed * (_run_speed_mult if _running else 1.0)
+		var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+		var move_direction = Input.get_axis("ui_left", "ui_right")
+		if Input.is_action_just_pressed("dodge") and can_dodge:
+			dodge_started_on_ground = is_on_floor()
+			if !dodge_started_on_ground:
+				velocity.x = direction.x * DODGE_SPEED
+				velocity.y = direction.y * DODGE_SPEED
+			else:
+				velocity.x = move_direction * DODGE_SPEED
+				if direction.y < 0:
+					velocity.y = direction.y * DODGE_SPEED
+					velocity.x *= 0.75
+			dodge_timer.start(0.5)
+			dodging = true
+			can_dodge = false
+		if move_direction:
+			velocity.x = move_direction * (RUN_SPEED if running else SPEED)
 		else:
-			velocity.x = move_toward(velocity.x, 0, _run_speed)
-
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+		old_velocity = velocity
+	else:
+		if is_on_floor() and !dodge_started_on_ground:
+			dodge_timer.stop()
+		else:
+			velocity.x *= 0.9
+			velocity.y *= 0.9
 	move_and_slide()
